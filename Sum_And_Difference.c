653f6dbe5192a8e367d319bc1fb375a08dae82f5
Arman Patel <89066881+Arman6117@users.noreply.github.com>
2022-12-15 12:44:39 +0530

    Sum and DIfference

---
+++ Sum_And_Difference.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+ int a , b;
+
+ scanf("%d", &a);
+ scanf("%d", &b);
+
+  float x, y;
+ 
+ scanf("%f", &x);
+ scanf("%f",  &y);
+
+ int sum = a + b;
+ int diff = a - b;
+
+ float sum1 = x + y;
+ float diff1 = x - y;
+ 
+ printf("%d %d\n", sum, diff);
+
+ printf("%0.1f %0.1f", sum1, diff1);
+
+
+
+
+
+
+
+  return.0;
+}
